542eac29c03d5a3c8be39948a519002ff7511e0c
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 18:36:02 -0300

    APICARS-01: Fix Autowired build error

--- src/main/java/com/challenge/apicars/application/mappings/user/UserMapping.java
+++ src/main/java/com/challenge/apicars/application/mappings/user/UserMapping.java
@@ -1,8 +1,11 @@
 package com.challenge.apicars.application.mappings.user;
 
+import org.springframework.stereotype.Service;
+
 import com.challenge.apicars.domain.entities.user.User;
 import com.challenge.apicars.domain.entities.user.UserDTO;
 
+@Service
 public class UserMapping {
 	
 	public UserDTO toDTO(User user) {
