611c3397e78fbb8e5caa3803b040cf558cc60a20
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 16:18:38 -0300

    APICARS-01: Create User Service

---
+++ src/main/java/com/challenge/apicars/application/services/user/UserService.java
@@ -0,0 +1,24 @@
+package com.challenge.apicars.application.services.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.challenge.apicars.application.mappings.user.UserMapping;
+import com.challenge.apicars.domain.entities.user.User;
+import com.challenge.apicars.domain.entities.user.UserDTO;
+import com.challenge.apicars.infra.repositories.user.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private UserMapping mapping;
+
+	public UserDTO singUp(UserDTO newUser) throws Exception {
+		User result = this.repository.save(this.mapping.toUser(newUser));
+		return this.mapping.toDTO(result);
+	}
+}
