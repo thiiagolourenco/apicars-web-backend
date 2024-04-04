ac2ad699656240a432766403a5e7780da0228854
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 01:11:41 -0300

    APICARS-00: Create entities files

---
+++ src/main/java/com/challenge/apicars/domain/car/Car.java
@@ -0,0 +1,8 @@
+package com.challenge.apicars.domain.car;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Car {
+
+}
---
+++ src/main/java/com/challenge/apicars/domain/user/User.java
@@ -0,0 +1,8 @@
+package com.challenge.apicars.domain.user;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+}
