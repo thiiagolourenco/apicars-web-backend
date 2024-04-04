b41bb5a7f984b5121b87ad33e56191ae66c65e6b
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 19:03:55 -0300

    APICARS-01: Create User Controller

---
+++ src/main/java/com/challenge/apicars/presentation/controllers/user/UserController.java
@@ -0,0 +1,25 @@
+package com.challenge.apicars.presentation.controllers.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.challenge.apicars.application.services.user.UserService;
+import com.challenge.apicars.domain.entities.user.UserDTO;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private  UserService service;
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> singUp(@RequestBody UserDTO newUser) {
+		return new ResponseEntity<UserDTO>(this.service.singUp(newUser), HttpStatus.CREATED);
+	}
+}
