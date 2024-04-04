b35d1f8ab80ced9dedeecf08fa284df6272d79a4
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 19:32:21 -0300

    APICARS-01: Add created user date in service logic

--- src/main/java/com/challenge/apicars/application/services/user/UserService.java
+++ src/main/java/com/challenge/apicars/application/services/user/UserService.java
@@ -1,5 +1,7 @@
 package com.challenge.apicars.application.services.user;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,7 @@ public class UserService {
 	private UserMapping mapping;
 
 	public UserDTO singUp(UserDTO newUser) {
+		newUser.setCreatedAt(LocalDateTime.now());
 		User result = this.repository.save(this.mapping.toUser(newUser));
 		return this.mapping.toDTO(result);
 	}
