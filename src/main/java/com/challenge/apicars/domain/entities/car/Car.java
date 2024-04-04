9d64277c00d333a8fc9a0be33f23a91fe512febe
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 12:43:44 -0300

    APICARS-01: Create entities attributes

--- src/main/java/com/challenge/apicars/domain/entities/car/Car.java
+++ src/main/java/com/challenge/apicars/domain/entities/car/Car.java
@@ -1,8 +1,23 @@
 package com.challenge.apicars.domain.entities.car;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
-@Entity
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="car")
+@Table(name="cars")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
 public class Car {
 
+	private Integer year;
+	private String licensePlate;
+	private String model;
+	private String color;
 }
--- src/main/java/com/challenge/apicars/domain/entities/user/User.java
+++ src/main/java/com/challenge/apicars/domain/entities/user/User.java
@@ -1,8 +1,34 @@
 package com.challenge.apicars.domain.entities.user;
 
-import jakarta.persistence.Entity;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
+import com.challenge.apicars.domain.entities.car.Car;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
 public class User {
 
+	private String firstName;
+	private String lastName;
+	private String email;
+	private LocalDate birthday;
+	private String login;
+	private String password;
+	private String phone;
+	
+	private List<Car> cars = new ArrayList<Car>();
 }
