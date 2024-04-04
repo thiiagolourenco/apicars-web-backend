9b2270efea26942cae2693fcc39b5ebf6291b487
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-04 19:04:40 -0300

    APICARS-01: Add findUserById method in User Repository

--- src/main/java/com/challenge/apicars/infra/repositories/user/UserRepository.java
+++ src/main/java/com/challenge/apicars/infra/repositories/user/UserRepository.java
@@ -1,8 +1,11 @@
 package com.challenge.apicars.infra.repositories.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.challenge.apicars.domain.entities.user.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
+	Optional<User> findUserById(Long id);
 }
