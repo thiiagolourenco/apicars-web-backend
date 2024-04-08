80bf309dcf240790eed29568793bfcc8705d2db8
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-08 14:12:24 -0300

    APICARS-00: Add swagger dependency and configuration

--- src/main/java/com/challenge/apicars/infra/security/SecurityConfigurations.java
+++ src/main/java/com/challenge/apicars/infra/security/SecurityConfigurations.java
@@ -20,6 +20,18 @@ public class SecurityConfigurations {
 	@Autowired
 	SecurityFilter securityFilter;
 
+	private static final String[] SWAGGER_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity.csrf(csrf -> csrf.disable())
@@ -31,12 +43,15 @@ public class SecurityConfigurations {
 						.requestMatchers(HttpMethod.GET, "/api/users/{id}").permitAll()
 						.requestMatchers(HttpMethod.DELETE, "/api/users/{id}").permitAll()
 						.requestMatchers(HttpMethod.PUT, "/api/users/{id}").permitAll()
-						.requestMatchers(HttpMethod.POST, "/api/cars").permitAll()
-						.requestMatchers(HttpMethod.GET, "/api/cars/by-user/{userId}").permitAll()
-						.requestMatchers(HttpMethod.GET, "/api/cars/{id}").permitAll()
-						.requestMatchers(HttpMethod.DELETE, "/api/cars/{id}").permitAll()
-						.requestMatchers(HttpMethod.PUT, "/api/cars/{id}").permitAll()
+						.requestMatchers(SWAGGER_WHITELIST).permitAll()
 						.anyRequest().authenticated())
+						/*
+						 * .requestMatchers(HttpMethod.POST, "/api/cars").permitAll()
+						 * .requestMatchers(HttpMethod.GET, "/api/cars/by-user/{userId}").permitAll()
+						 * .requestMatchers(HttpMethod.GET, "/api/cars/{id}").permitAll()
+						 * .requestMatchers(HttpMethod.DELETE, "/api/cars/{id}").permitAll()
+						 * .requestMatchers(HttpMethod.PUT, "/api/cars/{id}").permitAll()
+						 */
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
 	}
 
